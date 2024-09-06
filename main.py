373913a2f95993fac1beb2a9c16d9721cdc2a7ea
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-09-06 22:21:56 +0300

    выполнено

---
+++ main.py
@@ -0,0 +1,10 @@
+def add_everything_up(a,b):
+    try:
+        print(a+b)
+    except:
+        print(f'{a}{b}')
+
+
+add_everything_up(23,'rrrr')
+add_everything_up(45,'бабка ягодка опять')
+add_everything_up(23,43)
